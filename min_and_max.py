4d72d8faf71149aa2117047981506794fa928b4e
jeffekwan2 <93186320+jeffekwan2@users.noreply.github.com>
2021-11-12 12:14:55 +0800

    Add files via upload

---
+++ min_and_max.py
@@ -0,0 +1,6 @@
+a = float(input('input number a :'))
+b = float(input('input number b :'))
+c = float(input('input number c :'))
+
+
+print('the largest number is',max(a,b,c),'and the smallest number is',min(a,b,c))
